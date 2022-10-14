61337f48b6d93885a818943d027e3a6103800f37
Bakuk <bakuk1996@mail.ru>
2022-10-14 17:56:54 +0700

    add task

---
+++ task/Program.cs
@@ -0,0 +1,25 @@
+﻿int len = 10;
+string[] array = { "Hello", "2", "world", ":-)" };
+string[] newArr = new string[len];
+
+string[] ConvertArray(string[] arr){
+    string[] newArr = new string[arr.Length];
+    int j = 0;
+    for (int i = 0; i< arr.Length; i++){
+        string temp = arr[i];
+        if(temp.Length <= 3){
+            newArr[j] = temp;
+            j++;
+        }
+    }
+    return newArr;
+}
+
+void PrintArray(string[] arr){
+    for(int i = 0; i<arr.Length; i++){
+        Console.WriteLine(arr[i]);
+    }
+}
+
+newArr = ConvertArray(array);
+PrintArray(newArr);
